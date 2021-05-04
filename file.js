bb90088d2d71d62fd3cdf24e3027fa0e292cee79
viiiraj07 <58074485+viiiraj07@users.noreply.github.com>
2021-05-04 15:08:51 +0530

    Add new update
    Added new update which enables user to guess a number and then the web app displays if the guessed number is correct or not!

--- file.js
+++ file.js
@@ -1,25 +1,39 @@
 function refresh() {
+    document.getElementById('result-text').classList.remove('hvr-pulse-grow');
     let n = Math.floor(Math.random() * 6 + 1);
-    switch (n) {
-        case 1:
-            document.getElementById('dice-image').setAttribute("src", "dice1.png");
-            break;
-        case 2:
-            document.getElementById('dice-image').setAttribute("src", "dice2.png");
-            break;
-        case 3:
-            document.getElementById('dice-image').setAttribute("src", "dice3.png");
-            break;
-        case 4:
-            document.getElementById('dice-image').setAttribute("src", "dice4.png");
-            break;
-        case 5:
-            document.getElementById('dice-image').setAttribute("src", "dice5.png");
-            break;
-        case 6:
-            document.getElementById('dice-image').setAttribute("src", "dice6.png");
-            break;
-        default:
-            break;
+
+    let guess = document.getElementById('guess').value;
+    if (guess == "") {
+        document.getElementById('result-text').innerText = "ENTER GUESS NUMBER";
+    } else {
+        switch (n) {
+            case 1:
+                document.getElementById('dice-image').setAttribute("src", "dice1.png");
+                break;
+            case 2:
+                document.getElementById('dice-image').setAttribute("src", "dice2.png");
+                break;
+            case 3:
+                document.getElementById('dice-image').setAttribute("src", "dice3.png");
+                break;
+            case 4:
+                document.getElementById('dice-image').setAttribute("src", "dice4.png");
+                break;
+            case 5:
+                document.getElementById('dice-image').setAttribute("src", "dice5.png");
+                break;
+            case 6:
+                document.getElementById('dice-image').setAttribute("src", "dice6.png");
+                break;
+            default:
+                break;
+        }
+        if (n == guess) {
+            document.getElementById('result-text').innerText = "WINNER";
+            document.getElementById('result-text').classList.add('hvr-pulse-grow');
+        } else {
+            document.getElementById('result-text').innerText = "TRY AGAIN!";
+        }
     }
+
 }
